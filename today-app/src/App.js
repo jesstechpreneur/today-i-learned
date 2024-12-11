26811b56d337bc4e092fcfd91bb577818178eb2b
Jessica Johnson <hi@jesstechpreneur.com>
2024-12-10 23:27:58 -0600

    Updated CSS and Header

--- today-app/src/App.js
+++ today-app/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <h1> Hello World</h1>;
-}
+import "./style.css";
 
+function App() {
+  return (
+    <header>
+      <div className="logo">
+        <img
+          src="logo.png"
+          height="68"
+          width="68"
+          alt="Logo for What I Learned Today"
+        />
+        <h1>Today I Learned!!!!</h1>
+      </div>
+      <button className="btn post btn-lg">Share a Fact</button>
+    </header>
+  );
+}
 export default App;
